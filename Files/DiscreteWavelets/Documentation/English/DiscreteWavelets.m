b7b9bb9ae534b15361175cd6c2329fa3c1c29247
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2017-03-20 09:25:31 -0600

    Remove installer files

--- Files/DiscreteWavelets/Documentation/English/DiscreteWavelets.m
+++
@@ -1,5 +0,0 @@
-{"Name" -> "DiscreteWavelets", "Directory" -> "C:\\Documents and \
-Settings\\All Users\\Application \
-Data\\Mathematica\\Applications\\DiscreteWavelets\\Documentation\\English\\", 
- "Files" -> {"Applications.nb", "Overview.nb", "TransformsTools.nb", 
-   "Visualization.nb"}}
